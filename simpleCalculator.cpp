b98982dc1c831a74f2373e73ad8652c267c88009
Aristid <aristidg@gmail.com>
2025-10-29 15:38:27 +0100

    multiply-divide functionality

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -2,6 +2,19 @@
 #include <cstdlib>
 #include <cmath>
 
+double multiply(double a, double b) {
+    return a*b;
+}
+
+double divide(double a, double b) {
+    if (b == 0) {
+        return 0.0;
+    }
+    else {
+        return a/b;
+    }
+}
+
 int main (int argc, char** argv)
 {
 
