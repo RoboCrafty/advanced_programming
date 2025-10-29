99a6049a3129efb20f04a64b2e72d347ac9caef4
Banday, Fuzail Hamid (FT RPD CED INW-DE) <fuzail-hamid.banday@siemens.com>
2025-10-29 15:36:34 +0100

    Impliment sum and sub

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -2,9 +2,19 @@
 #include <cstdlib>
 #include <cmath>
 
+double sum (double a, double b)
+{
+    return a + b;
+}
+
+double subtract (double a, double b)
+{
+    return a - b;
+}
+
 int main (int argc, char** argv)
 {
 
-    
+
 
 }
