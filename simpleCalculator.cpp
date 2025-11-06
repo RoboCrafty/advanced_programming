6039e95af47d22fc8c3931f011e8918e12a65900
RoboCrafty <fuzailhamid2002@gmail.com>
2025-11-06 22:03:19 +0100

    Implement menu

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -3,35 +3,71 @@
 #include <cmath>
 
 
-double sum (double a, double b)
+double sum (double &a, double &b)
 {
     return a + b;
 }
 
-double subtract (double a, double b)
+double subtract (double &a, double &b)
 {
     return a - b;
 
 }
 
-double multiply(double a, double b) {
+double multiply(double &a, double &b) {
     return a*b;
 }
 
-double divide(double a, double b) {
+double divide(double &a, double &b) {
     if (b == 0) {
         return 0.0;
     }
     else {
-        return a/b;
+        return a / b;
+    }
+}
+
+double menu (char& opr, double &a, double &b)
+{
+    switch (opr)
+    {
+    case '+':
+        return sum(a, b);
+        break;
+    case '-':
+        return subtract(a, b);
+        break;
+    case '*':
+        return multiply(a, b);
+        break;
+    case '/':
+        return divide(a, b);
+        break;
+    default:
+        break;
     }
 
-
+    return 0;
 }
 
 int main (int argc, char** argv)
 {
+    char opr;
+    double a, b;
+    std::cout << "Enter operand (+, -, *, /): ";
+    std::cin >> opr;
+    std::cout << std::endl;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+    std::cout << std::endl;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+    std::cout << std::endl;
+
+    std::cout << "Result = " << menu(opr, a, b) << std::endl;
 
 
-
+    return 0;
 }
